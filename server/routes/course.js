43d8ab68898a9850e5ac0165398c6c33d31f9d13
tingul4 <xpple413208@gmail.com>
2023-11-02 17:24:06 +0800

    feat: add RUD course routes

--- server/routes/course.js
+++ server/routes/course.js
@@ -7,6 +7,32 @@ router.use((req, res, next) => {
   next();
 });
 
+// 獲得系統中的所有課程
+router.get("/", async (req, res) => {
+  try {
+    let courseFound = await Course.find({})
+      .populate("instructor", ["username", "email"])
+      .exec();
+    return res.send(courseFound);
+  } catch (e) {
+    return res.status(5000).send(e);
+  }
+});
+
+// 用課程id尋找課程
+router.get("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let courseFound = await Course.findOne({ _id })
+      .populate("instructor", ["email"])
+      .exec();
+    return res.send(courseFound);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+// 新增課程
 router.post("/", async (req, res) => {
   // 驗證數據符合規範
   let { error } = courseValidation(req.body);
@@ -30,6 +56,59 @@ router.post("/", async (req, res) => {
     return res.send("新課程已經保存");
   } catch (e) {
     return res.status(500).send("無法創建課程。。。");
+  }
+});
+
+// 更改課程
+router.patch("/:_id", async (req, res) => {
+  // 驗證數據符合規範
+  let { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let { _id } = req.params;
+  // 確認課程存在
+  try {
+    let courseFound = await Course.findOne({ _id });
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法更新課程內容。");
+    }
+
+    // 使用者必須是此課程講師，才能編輯課程
+    if (courseFound.instructor.equals(req.user._id)) {
+      let updatedCourse = await Course.findOneAndUpdate({ _id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      return res.send({
+        message: "課程已經被更新成功",
+        updatedCourse,
+      });
+    } else {
+      return res.status(403).send("只有此課程的講師才能編輯課程。");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  // 確認課程存在
+  try {
+    let courseFound = await Course.findOne({ _id }).exec();
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法刪除課程。");
+    }
+
+    // 使用者必須是此課程講師，才能刪除課程
+    if (courseFound.instructor.equals(req.user._id)) {
+      await Course.deleteOne({ _id }).exec();
+      return res.send("課程已被刪除。");
+    } else {
+      return res.status(403).send("只有此課程的講師才能刪除課程。");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
   }
 });
 
