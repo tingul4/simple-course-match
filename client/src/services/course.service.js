45dfc42fb0c37c0ce55db509ebe0b9bef6ae7556
tingul4 <xpple413208@gmail.com>
2023-11-10 21:44:50 +0800

    feat: init for deployment on GCP

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://localhost:8080/api/courses'
+const API_URL = 'https://simple-course-match-malyixlpwq-de.a.run.app/api/courses'
 
 class CourseService {
   constructor () {
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = 8080
 const { auth, course } = require('./routes')
 const passport = require('passport')
 require('./config/passport')(passport)
 const cors = require('cors')
+const path = require('path')
+const port = process.env.PORT || 8080
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mernDB')
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('connect to mongodb successfully!')
   })
@@ -18,8 +19,13 @@ mongoose
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.use('/api/user', auth)
 app.use('/api/courses', passport.authenticate('jwt', { session: false }), course) // need JWT to authorize
 
-app.listen(PORT, () => console.log(`backend server is listening on PORT ${PORT}...`))
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')))
+}
+
+app.listen(port, () => console.log(`backend server is listening on PORT ${port}...`))
